3ef2dcfe90eaa7b550b96598c091a5508868ab67
Kevin Fraser <kevin@ksfraser.com>
2026-05-15 08:00:09 -0600

    fix: add version property and composer install on activation

--- hooks.php
+++ hooks.php
@@ -7,6 +7,7 @@ define('SS_EMAIL', 126 << 8);
 
 class hooks_fa_emailmanager extends hooks {
     var $module_name = 'fa_emailmanager';
+    var $version = '1.0.0';
 
     function install_options($app) {
         global $path_to_root;
